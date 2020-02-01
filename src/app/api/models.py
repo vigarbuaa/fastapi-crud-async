539e933e20d97e2f6f2d8ee06d2fd2ca93992b61
vigarbuaa <vigarxueer@126.com>
2020-02-01 11:07:57 +0800

    add personschema pydantic

--- src/app/api/models.py
+++ src/app/api/models.py
@@ -5,6 +5,14 @@ class NoteSchema(BaseModel):
     title: str = Field(..., min_length=3, max_length=50)
     description: str = Field(..., min_length=3, max_length=50)
 
+class PersonSchema(BaseModel):
+    name: str = Field(..., min_length=3, max_length=50)
+    idcard: str = Field(..., min_length=3, max_length=50)
+    hometown: str = Field(..., min_length=3, max_length=50)
+    address: str = Field(..., min_length=3, max_length=50)
 
 class NoteDB(NoteSchema):
+    id: int
+
+class PersonDB(PersonSchema):
     id: int
